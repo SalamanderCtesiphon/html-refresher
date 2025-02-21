002d8b03355015bf50fea33cbc70b4bb4e8b72a7
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-21 01:36:06 -0600

    installed nvm and tested out the javascript file

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello")
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+function favoriteAnimal(animal) {
+  return animal + " is my favorite animal!"
+}
+
+console.log(favoriteAnimal("Goat"))
